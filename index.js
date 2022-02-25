8dca57bdaaaad1145d0cf68c938d9985308791fe
Spiegel <turbogara1@gmail.com>
2022-02-25 20:02:35 +0900

    add:console

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
 
 app.get('/test1', (req, res) => {
   res.send("this is test1")
+  console.log('request: ', req)
+  console.log('response: ', res)
 })
 
 app.listen(port, () => {
