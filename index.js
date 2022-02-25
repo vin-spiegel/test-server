640e73a3fe9203dc490ec9476d57f6598ea3711f
Spiegel <turbogara1@gmail.com>
2022-02-25 18:57:35 +0900

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const test = "Hello World!"
+const test = "Hello World!!"
 
 app.get('/', (req, res) => {
   res.send(test)
